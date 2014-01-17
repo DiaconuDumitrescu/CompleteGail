6d06fc07a0321bac330b03c503ffdfc7ab60bc42
sorinqoffice <sorin@qoffice.ro>
2014-01-17 17:50:00 +0200

    Good

--- Gail/SplashPage.xaml.cs
+++ Gail/SplashPage.xaml.cs
@@ -8,39 +8,43 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using System.IO;
+using System.Threading;
+
 
 namespace Gail
 {
     public partial class SplashPage : PhoneApplicationPage
     {
-        private HttpWebRequest request;
-
+        //private HttpWebRequest reques
         public SplashPage()
         {
+        
             InitializeComponent();
-            Loaded += SplashPage_Loaded;
+            this.Loaded += SplashPage_Loaded;
         }
 
         void SplashPage_Loaded(object sender, RoutedEventArgs e)
         {
-            request = (HttpWebRequest)HttpWebRequest.Create(new Uri("http://www.developer.nokia.com/Community/Wiki/Portal:Windows_Phone_UI_Articles"));
-            request.BeginGetResponse(new AsyncCallback(ReceiveResponseCallBack), null);
+            Thread.Sleep(1500);
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+          //  request = (HttpWebRequest)HttpWebRequest.Create(new Uri("http://www.developer.nokia.com/Community/Wiki/Portal:Windows_Phone_UI_Articles"));
+          //  request.BeginGetResponse(new AsyncCallback(ReceiveResponseCallBack), null);
         }
 
         private void ReceiveResponseCallBack(IAsyncResult result)
         {
-            HttpWebResponse response = (HttpWebResponse)this.request.EndGetResponse(result);
-            using (var stream = response.GetResponseStream())
-            {
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    string str = reader.ReadToEnd();
-                    Deployment.Current.Dispatcher.BeginInvoke(() =>
-                    {
-                        NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
-                    });
-                }
-            }
+          // HttpWebResponse response = (HttpWebResponse)this.request.EndGetResponse(result);
+          //  using (var stream = response.GetResponseStream())
+          //  {
+          //      using (StreamReader reader = new StreamReader(stream))
+          //     {
+          //          string str = reader.ReadToEnd();
+          //          Deployment.Current.Dispatcher.BeginInvoke(() =>
+          //          {
+                       
+          //          });
+          //      }
+          //  }
         }
     }
 }
